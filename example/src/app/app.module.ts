6b612e090c4dc75da65025b0d3663ad40e9157cd
Singularity-one <52284919+Singularity-one@users.noreply.github.com>
2019-12-13 13:13:48 +0800

    master2.7

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 //import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [ProductComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule,FormsModule],
     providers: [],
     bootstrap: [ProductComponent]
 })
